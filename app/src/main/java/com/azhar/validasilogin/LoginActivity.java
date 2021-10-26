75e4a42b9f9e481625077a0d3775996cc1f50a9c
irsyadfkrz <irsyadfkrz10@gmail.com>
2021-10-26 13:11:09 +0700

    Initial commit

---
+++ app/src/main/java/com/azhar/validasilogin/LoginActivity.java
@@ -0,0 +1,119 @@
+package com.azhar.validasilogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String FILENAME = "login";
+
+    EditText editUsername, editPassword;
+    Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedinstanceStatic) {
+        super.onCreate(savedinstanceStatic);
+        setContentView(R.layout.activity_login);
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+        btnRegister = findViewById(R.id.action_register);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,
+                        RegisterActivity.class);
+                StartActivity(intent);
+            }
+        });
+
+    }
+
+
+    void simpanFileLogin() {
+        String isiFile = editUsername.getText().toString() + ";" + editPassword.getText().toString();
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            File.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Login Berhasil",
+                Toast.LENGTH_SHORT).show();
+        onBackPressed();
+    }
+
+    void login() {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard,
+                editUsername.getText().toString());
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br =
+                        new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Error " + e.getMessage());
+            }
+        }
+        String data = text.toString();
+        String() dataUser = data.split(";");
+
+        if (dataUser(1).equals(editPassword.getText().toString())) {
+            simpanFileLogin();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Password Tidak Sesuai",
+                    Toast.LENGTH_SHORT).show();
+        }
+    } else
+
+    {
+        Toast.makeText(this, "User Tidak Ditemukan", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
